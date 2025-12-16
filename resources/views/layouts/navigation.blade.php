53df1ff4b61d93c63a3e31d1f001aed8545d88a9
Aaron Dills <aaronsdills@gmail.com>
2025-12-16 14:19:23 -0500

    Fix label editing/deleting, and update logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('home') }}" class="flex items-center gap-2">
+                        <x-application-logo class="block h-8 w-8 text-blue-600" />
+                        <span class="text-xl font-bold text-gray-900 dark:text-white">TaskFlow</span>
                     </a>
                 </div>
 
